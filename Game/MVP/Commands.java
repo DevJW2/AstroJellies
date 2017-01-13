0d365194c10e4371adba5d96c00c512d50104dc3
Jeffrey Weng <jweng2@stuy.edu>
2017-01-12 22:17:53 -0500

    setup commands, VFD, Woo, for basic directory management

--- Game/MVP/Commands.java
+++ Game/MVP/Commands.java
@@ -1,5 +1,21 @@
 public class Commands{
-    public static void main(String[] args){
 
+    public static void changeDirectory(String input){
+	if(input == "/root" && (input != VFD.currentDir )){
+	    VFD.currentDir = "root";
+	    
+	}
+	else if(input == "/tutorial" && (input != VFD.currentDir)){
+	    VFD.currentDir = "tutorial";
+	}
     }
+    public static void listDirectory(){
+	if (VFD.currentDir.equals("root")){
+	    System.out.println(VFD.rootDirectory());
+	    }
+	else if(VFD.currentDir.equals("tutorial")){
+	    System.out.println(VFD.tutorialD());
+	}
+    }
+    
 }
--- Game/MVP/Woo.java
+++ Game/MVP/Woo.java
@@ -1,6 +1,67 @@
+import cs1.Keyboard;
+
 public class Woo{
+    
+    //root, tutorial, home, etc
+    public static boolean value = true; 
+
 	public static void main(String[] args){
-		System.out.println("Hello");
+		System.out.println("Welcome....");
+		System.out.println("Please enter you name: ");
+		String name = Keyboard.readString();
+		System.out.println("Welcome Hacker: " + name);
+		System.out.println("BACKSTORY");
+		System.out.println("Initiating Game....");
+		System.out.println("Loading Tutorial....");
+		System.out.println("");
+		System.out.println("Entering root directory..");
+		while(value == true){
+		    System.out.println("--------");
+		    System.out.println( VFD.getcurrentDir());
+		    
+		    String userInput = Keyboard.readString();
+		    if(userInput.equals("cd/root")){
+			System.out.println("");
+			Commands.changeDirectory("/root");
+		    }
+		    
+		    else if(userInput.equals("cd/tutorial")){
+			System.out.println("");
+			Commands.changeDirectory("/tutorial");
+		    }
+		    else if(userInput.equals("ls")){
+			System.out.println("");
+			Commands.listDirectory();
+			}
+		    else if(userInput.equals("help")){
+			System.out.println("");
+			System.out.println("list directory files: ls");
+			System.out.println("-");
+			System.out.println("change directory: cd/....");
+			System.out.println("-");
+			System.out.println("NOT PART OF GAME");
+			System.out.println("Admin....");
+			System.out.println("To exit out of loop: exit");
+		    }
+		    else if(userInput.equals("exit")){
+			value = false;
+		    }
+		    else{
+		        System.out.println("Please insert a viable command");
+			System.out.println("Try using the help command...");
+		    }
+		}
+		
+
+		
+
+		//set up basic tutorial
+		//create basic puzzle
+		//create basic commands
+		//create VFD
+		//Solve puzzle
+		//go to scene 1
+
 	}
 
 }
