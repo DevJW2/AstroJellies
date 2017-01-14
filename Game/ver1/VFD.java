6cbed11f0a275b6949f293b29e419fd07f4930bc
Jeffrey Weng <jweng2@stuy.edu>
2017-01-14 15:12:54 -0500

    added new version

---
+++ Game/ver1/VFD.java
@@ -0,0 +1,29 @@
+public class VFD{
+    //arraylist with all directories, later on
+    public static String currentDir = "root";
+    public static int currentLayer = 0;
+
+    public static String getcurrentDir(){
+	return currentDir; 
+    }
+    
+    public static void checkState(){
+	System.out.println(currentDir);
+    }
+
+    public static String rootDirectory(){
+	currentLayer = 0;
+	String retStr = "";
+	retStr += "/user\n";
+	retStr += "/tutorial";
+	return retStr;
+	
+    }
+    public static String tutorialD(){
+	currentLayer = 1;
+	String retStr = "";
+	retStr += "largeTextFile.txt\n";
+	retStr += "playGame.exe";
+	return retStr;
+    }
+}
